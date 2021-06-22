70089502c217a027434adeadf47593a21a8b814e
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-23 00:30:10 +0530

    Added: Messaage Constructor

---
+++ src/MoodAnalyzer/CheckMood.java
@@ -0,0 +1,25 @@
+package MoodAnalyzer;
+/************************************************************
+ * Purpose : Create a mood analyser program 
+ * @author Sanyukta Nirmal
+ * @Version 1.0
+ * @since 22-06-2021
+ *
+ ************************************************************/
+
+
+public class CheckMood {
+	
+	/**
+	 * check message and print sad else happy 
+	 * @param message contain sad
+	 * @return sad or happy
+	 */
+	public String Mood(String message) {
+		if(message.contains("Sad") )
+			return "Sad";
+		else
+			return "Happy";			
+	}
+
+}
